ef3b363fbcd6cb1d0ad8ff6075a44dc1f895fc7c
Shane Mc Cormack <shanemcc@gmail.com>
2017-11-17 13:02:30 +0000

    Add missing file.

---
+++ tasks/workers/job_sequence.php
@@ -0,0 +1,28 @@
+<?php
+	/**
+	 * Task to run multiple jobs in sequence, one after another.
+	 *
+	 * Payload is a json string with a 'jobs' array where each item has "job"
+	 * and "args" arrays.
+	 *
+	 * "job" will be used as a job to run, and "args" will be passed in as the
+	 * payload.
+	 */
+	class job_sequence extends TaskWorker {
+		public function run($job) {
+			$payload = $job->getPayload();
+
+			if (isset($payload['jobs'])) {
+				foreach ($payload['jobs'] as $newJob) {
+					if (!isset($newJob['job']) || !isset($newJob['args'])) { continue; }
+
+					$newjob = new JobInfo('', $newJob['job'], $newJob['args']);
+					$this->getTaskServer()->runJob($newjob);
+				}
+
+				$job->setResult('OK');
+			} else {
+				$job->setError('Missing fields in payload.');
+			}
+		}
+	}
