a26122d583538306b3d8ba5af27a352c94e16b66
Shane Mc Cormack <shanemcc@gmail.com>
2019-07-29 19:48:15 +0100

    Split docs into separate apib files.

--- web/1.0/docs/generate.sh
+++ web/1.0/docs/generate.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
+DIR="$(dirname "$(readlink -f "$0")")"
+cd "${DIR}"
+
+if [ -e index.apib ]; then
+	rm index.apib
+fi;
+
+cat *.apib >index.apib 2>/dev/null
 aglio --theme-variables default --theme-full-width --no-theme-condense -i index.apib -o index.html
