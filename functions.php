420d22f7323745db4f9c2cf96d8b6252c082281c
Shane Mc Cormack <shanemcc@gmail.com>
2019-08-03 07:38:15 +0100

    Add a trailing : to the redis session prefix.

--- functions.php
+++ functions.php
@@ -221,7 +221,7 @@
 
 		if (isset($config['redis']) && !empty($config['redis'])) {
 			ini_set('session.save_handler', 'redis');
-			ini_set('session.save_path', 'tcp://' . $config['redis'] . ':' . $config['redisPort'] . '/?prefix=' . $config['redisSessionPrefix']);
+			ini_set('session.save_path', 'tcp://' . $config['redis'] . ':' . $config['redisPort'] . '/?prefix=' . urlencode($config['redisSessionPrefix'] . ':'));
 		} else if (isset($config['memcached']) && !empty($config['memcached'])) {
 			ini_set('session.save_handler', 'memcached');
 			ini_set('session.save_path', $config['memcached']);
