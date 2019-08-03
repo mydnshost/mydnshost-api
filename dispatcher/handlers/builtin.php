2bce827ffbc38873d3facf63b30d5fbfaddd87cc
Shane Mc Cormack <shanemcc@gmail.com>
2019-08-03 20:16:33 +0100

    Typo

--- dispatcher/handlers/builtin.php
+++ dispatcher/handlers/builtin.php
@@ -4,7 +4,7 @@
 		$gmc->doBackground('sendmail', json_encode(['to' => $to, 'subject' => $subject, 'message' => $message, 'htmlmessage' => $htmlmessage]));
 	});
 
-	HEventQueue::get()->subscribe('verify_2fa_push', function($key, $message) use ($gmc) {
+	EventQueue::get()->subscribe('verify_2fa_push', function($key, $message) use ($gmc) {
 		$gmc->doBackground('verify_2fa_push', json_encode(['keyid' => $key->getID(), 'userid' => $key->getUserID(), 'message' => $message]));
 	});
 
