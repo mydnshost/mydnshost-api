d44544de4f2c618e5a8012b8c619d27d3e5a2fe7
Shane Mc Cormack <shanemcc@gmail.com>
2019-08-04 21:26:57 +0100

    Bleh.

--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 set -e
 
-php /dnsapi/admin/init.php && exec "apache2-foreground"
+# # first arg is `-f` or `--some-option`
+# if [ "${1#-}" != "$1" ]; then
+# 	set -- php "$@"
+# fi
+
+if [ "${@}" == "apache2-foreground" ]; then
+	php /dnsapi/admin/init.php && exec "apache2-foreground"
+else
+	exec "$@"
+fi;
