mydnshost/mydnshost-api

commit dbb924a0592e54488f99f827f0edb6c8fb953408

Allow RRCLONE records to point at other domains.

web/experimental/httpreq/methods/httpreq.php
@@ -19,39 +19,6 @@
 			return true;
 		}
 
-		protected function findDomain($domain) {
-			$domains = [];
-
-			// Remove trailing .
-			$domain = rtrim($domain, '.');
-
-			// Convert the requested domain into an array (eg foo.bar.baz.example.com => [foo, bar, baz, example, com])
-			$bits = explode('.', $domain);
-
-			// Domains can have at most 255 characters,
-			// subdomains require a . between them leaving a maximum sub-domain count of ~128 levels deep.
-			// If someone tries to look for more than this then just ignore them.
-			$limit = 128;
-			do {
-				// Get the domain to look for ([foo, bar, baz, example, com] => foo.bar.baz.example.com)
-				$dom = implode('.', $bits);
-
-				// If we have an exact match for this domain, then only return it in the output.
-				// There may be a nicer way to do this than asking the DB every time.
-				$domain = $this->getContextKey('user')->getDomainByName($dom);
-				if ($domain !== FALSE) {
-					$domains[] = $domain;
-					break;
-				}
-
-				// Remove the first entry from the array so that the next time we check the parent domain.
-				// eg [foo, bar, baz, example, com] => [bar, baz, example, com] and the next check is bar.baz.example.com
-				array_shift($bits);
-			} while (!empty($bits) && $limit-- > 0);
-
-			return $domains;
-		}
-
 		public function doRun($deleteOnly) {
 			$user = $this->getContextKey('user');
 			$data = $this->getContextKey('data');
@@ -60,14 +27,10 @@
 
 			$wantedRecordFull = $wantedRecord = $data['fqdn'];
 
-			$domains = $this->findDomain($wantedRecord);
-			if (empty($domains)) {
+			$domain = $this->findDomain($this->getContextKey('user'), $wantedRecord);
+			if ($domain == FALSE) {
 				$this->getContextKey('response')->sendError('No matching domains found for: ' . $wantedRecord);
-			} else if (count($domains) > 1) {
-				$this->getContextKey('response')->sendError('Too many matching domains found for: ' . $wantedRecord);
 			}
-
-			$domain = $domains[0];
 
 			$this->checkAccess($domain, ['write', 'admin', 'owner']);
 

classes/domain.php
@@ -492,6 +492,10 @@ class Domain extends DBObject {
 
 		if (!$raw) {
 			// TODO: Maybe allow RRCLONE to reference other RRCLONE records eventually.
+
+			// Used to save us asking the DB for the same thing over and over
+			$rrCloneCache = [];
+
 			foreach ($cloneRecords as $record) {
 				$name = $this->fixRecordName($record, $recordDomain);
 				$content = $this->fixRecordContent($record, $recordDomain);
@@ -504,7 +508,41 @@ class Domain extends DBObject {
 					$importTypes = explode(',', strtoupper($m[1]));
 				}
 
-				foreach ($records->getByName($wantedRecord) as $sourceRecord) {
+				$sourceRecords = $records->getByName($wantedRecord);
+
+				if (empty($sourceRecords)) {
+					if (!isset($rrCloneCache[$wantedRecord])) {
+						$rrCloneCache[$wantedRecord] = [];
+
+						// Record doesn't exist in this domain, so check other
+						// domains.
+						$sourceDom = Record::findDomainForRecord($this->getDB(), $wantedRecord);
+						if ($sourceDom != FALSE) {
+							// Check if we have sufficient access.
+							$hasAccess = false;
+
+							// Is there anyone in our domain who has write access,
+							// who also has read-access to the source domain?
+							foreach ($this->_access as $id => $level) {
+								if (in_array($level, ['owner', 'admin', 'write'])) {
+									if (isset($sourceDom->_access[$id]) && in_array($sourceDom->_access[$id], ['owner', 'admin', 'write', 'read'])) {
+										$hasAccess = true;
+										break;
+									}
+								}
+							}
+
+							if ($hasAccess) {
+								// TODO: This feels inefficient.
+								$sourceRecordsInfo = $sourceDom->getRecordsInfo(false, true)['records'];
+								$rrCloneCache[$wantedRecord] = $sourceRecordsInfo->getByName($wantedRecord);
+							}
+						}
+					}
+					$sourceRecords = $rrCloneCache[$wantedRecord];
+				}
+
+				foreach ($sourceRecords as $sourceRecord) {
 					if (empty($importTypes) || in_array($sourceRecord['Type'], $importTypes) || in_array('*', $importTypes)) {
 						$records->addRecord($name, $sourceRecord['Type'], $sourceRecord['Address'], $sourceRecord['TTL'], $sourceRecord['Priority']);
 						$hasNS |= ($sourceRecord['Type'] == "NS" && $record->getName() == $recordDomain->getDomain());

classes/record.php
@@ -2,6 +2,7 @@
 
 use shanemcc\phpdb\DBObject;
 use shanemcc\phpdb\ValidationFailed;
+use shanemcc\phpdb\DB;
 
 class Record extends DBObject {
 	protected static $_fields = ['id' => NULL,
@@ -22,6 +23,43 @@ class Record extends DBObject {
 
 	public static function getValidRecordTypes() {
 		return Record::$VALID_RRs;
+	}
+
+	public static function findDomainForRecord($context, $record) {
+		// Remove trailing .
+		$record = rtrim($record, '.');
+
+		// Convert the requested domain into an array (eg foo.bar.baz.example.com => [foo, bar, baz, example, com])
+		$bits = explode('.', $record);
+
+		// Domains can have at most 255 characters,
+		// subdomains require a . between them leaving a maximum sub-domain count of ~128 levels deep.
+		// If someone tries to look for more than this then just ignore them.
+		$limit = 128;
+		do {
+			// Get the domain to look for ([foo, bar, baz, example, com] => foo.bar.baz.example.com)
+			$dom = implode('.', $bits);
+
+			// If we have an exact match for this domain, then only return it in the output.
+			// There may be a nicer way to do this than asking the DB every time.
+			var_dump($context);
+			if ($context instanceof DB) {
+				$domain = Domain::loadFromDomain($context, $dom);
+			} else if ($context instanceof User) {
+				$domain = $context->getDomainByName($dom);
+			} else {
+				$domain = FALSE;
+			}
+			if ($domain !== FALSE) {
+				return $domain;
+			}
+
+			// Remove the first entry from the array so that the next time we check the parent domain.
+			// eg [foo, bar, baz, example, com] => [bar, baz, example, com] and the next check is bar.baz.example.com
+			array_shift($bits);
+		} while (!empty($bits) && $limit-- > 0);
+
+		return FALSE;
 	}
 
 	public function __construct($db) {

commit 81713072a5e94b94a1f5ede712af9e139d0779bd

Fix bug introduced in dbb924a0592e54488f99f827f0edb6c8fb953408

web/experimental/httpreq/methods/httpreq.php
@@ -27,7 +27,7 @@
 
 			$wantedRecordFull = $wantedRecord = $data['fqdn'];
 
-			$domain = $this->findDomain($this->getContextKey('user'), $wantedRecord);
+			$domain = Record::findDomain($this->getContextKey('user'), $wantedRecord);
 			if ($domain == FALSE) {
 				$this->getContextKey('response')->sendError('No matching domains found for: ' . $wantedRecord);
 			}