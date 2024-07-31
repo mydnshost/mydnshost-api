e76a729ca20d98ce9a97fe39aa913f2b17bb1ceb
Shane Mc Cormack <shanemcc@gmail.com>
2024-07-31 01:06:48 +0100

    Add swagger-ified version of api docs for testing purposes. Probably not fit for purpose for all endpoints.

--- web/1.0/docs/generate.sh
+++ web/1.0/docs/generate.sh
@@ -9,3 +9,14 @@ fi;
 
 cat *.apib >index.apib 2>/dev/null
 aglio --theme-variables default --theme-full-width --no-theme-condense -i index.apib -o index.html
+
+# Also attempt-to-generate a swagger version.
+# This is not perfect, but probably good enough.
+docker run --user $(id -u) -it --rm -v $(pwd):/docs ghcr.io/kminami/apib2swagger -i /docs/index.apib -o /docs/swagger.yaml --open-api-3 --yaml
+
+# Fix security options in swagger version.
+sed -i '/^components:$/,+1d' swagger.yaml
+cat swagger-auth.yaml >> swagger.yaml
+
+# Fix relative url in swagger
+sed -i 's#https://api.mydnshost.co.uk/#/#g' swagger.yaml
